69b93fb059f65f65582be52e70da350f27a5ad73
Jean Carlo M. San Juan <jcsanjuan12@gmail.com>
2021-09-27 18:38:27 +0800

    added login forms to navbar

--- js/navbar.js
+++ js/navbar.js
@@ -4,25 +4,42 @@ const nav = document.getElementById("navbar");
 const pages = {
     "Home":"index.html",
     "About T.I.P.": "about.html",
-    "Program Offerings": "programs.html",
-    "ABET-Accredited Programs": "abet_programs.html", //Todo integrate abet with program offerings
     "Admissions": "admissions.html",
+    "Program Offerings": "programs.html",
     "T.I.P. TechnoCoRe": "https://technocore.tip.edu.ph/",
     "T.I.P. Career Center": "https://careercenter.tip.edu.ph/",
     "News": "news.html",
-    "Contact Us": "contacts.html"
+    "Contact Us": "contacts.html",
 };
 
-// Adds the company name in every page
-        
+//"ABET-Accredited Programs": "abet_programs.html", //Todo integrate abet with program offerings
+
+const login_forms = {
+    "Library": "https://library.tip.edu.ph/",
+    "T.I.P. E-mail": "https://tip.edu.ph/mail.php",
+    "ARIS": "https://tip.edu.ph/aris/"
+};
+
+function add_links(list) {
+    // Adds nav bar links based on the "pages" object
+    Object.keys(list).forEach(name => {
+        const href= pages[name];
+        nav.innerHTML += `<a href=${href}>${name}</a>`;
+    })
+}
+
+/* nav.innerHTML += `
+    <div>
+        Login:
+        <br>
+        <a href="https://library.tip.edu.ph/" target=_blank>LIBRARY</a>
+        <a href="https://tip.edu.ph/mail.php" target=_blank>T.I.P. EMAIL</a>
+        <a href="https://tip.edu.ph/aris/" target=_blank>ARIS</a>
+    </div
+`  */
+
+// Adds the ff: Logo, site links, and login forms
 nav.innerHTML += "<img src=media/logo.png>"
-
-// Adds nav bar links based on the "pages" object
-Object.keys(pages).forEach(name => {
-    const href= pages[name];
-    const title = document.title.toLocaleLowerCase();
-    const _name = name.toLocaleLowerCase();
-    const is_current = (title.search(_name) != -1) ? "id=current":"";
-
-    nav.innerHTML += `<a href=${href} ${is_current}>${name}</a>`;
-})
+add_links(pages);
+nav.innerHTML += "Login: <br>";
+add_links(login_forms);
