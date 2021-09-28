3811368dd800117d00ff4f1a419dd676af881fee
Jean Carlo M. San Juan <jcsanjuan12@gmail.com>
2021-09-28 17:29:21 +0800

    Fixed bug in the function add_links() which causes the link to be undefined

--- js/navbar.js
+++ js/navbar.js
@@ -10,30 +10,33 @@ const pages = {
     "T.I.P. Career Center": "https://careercenter.tip.edu.ph/",
     "News": "news.html",
     "Contact Us": "contacts.html",
-    "--":"",
+};
+
+const login_forms = {
     "Library": "library.tip.edu.ph/",
     "T.I.P. E-mail": "https://tip.edu.ph/mail.php",
     "ARIS": "https://tip.edu.ph/aris/"
-};
+} 
 
 //"ABET-Accredited Programs": "abet_programs.html", //Todo integrate abet with program offerings
 
 function add_links(list) {
     // Adds nav bar links based on the "pages" object
     let code = "";
-    Object.keys(list).forEach(name => {
-        if (name == "--") {
-            code +="<hr>"
-        } else {
-        const href = pages[name];
+    for (let name in list) {
+        const href = list[name];
         code += `<a href="${href}">${name}</a>`;
-        }
-    })
+    }
     return code;
 }
 
 // Adds the Logo, site links, and login forms to the nav bar.
-nav.innerHTML += `<img src=media/logo.png> ${add_links(pages)}`
+nav.innerHTML += `
+    <img src=media/logo.png> 
+    ${add_links(pages)}
+    <hr>
+    ${add_links(login_forms)}
+    `;
 
 const mobile_nav = document.getElementById("mobile-nav");
 mobile_nav.addEventListener("click", () => {
