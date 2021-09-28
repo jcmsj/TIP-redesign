5be0ba199c4ec7eea247426486237c9446f3720e
Jean Carlo M. San Juan <jcsanjuan12@gmail.com>
2021-09-28 21:14:40 +0800

    made custom select tag for CITE page

--- js/cite_qc.js
+++ js/cite_qc.js
@@ -1,28 +1,26 @@
-const nav2 = document.getElementById("nav2");
-
-const sub_links = {
-    "Outcomes-Based Education":"#OBE",
-    "Program Certification":"#PC",
-    "Associate in Computer Technology": "#ACT",
-    "Computer Science": "#CS",
-    "Information System":"#IS",
-    "Information Technology": "#IT"
-};
-
-let displayed = document.querySelector("#OBE");
-
-nav2.innerHTML += add_links(sub_links);
-
-document.querySelectorAll("#nav2 a").forEach( (item, i) => {
-    item.addEventListener("click", e => {
-        displayed.toggleAttribute("active")
-        for (let key in sub_links) {
-            let val = sub_links[key]
-            if (item.href.search(val) > -1) {
-                displayed = document.querySelector(val);
-                displayed.toggleAttribute("active");
-                return;
-            }
-        }
+let displayed = document.getElementById("OBE");
+const options = document.querySelectorAll("option")
+const sec = document.getElementById("section-handler");
+const summary = document.querySelector("summary");
+options.forEach( option => {
+    option.addEventListener("click", e => {
+        summary.innerText = option.innerText;
+        display()
+        displayed = document.getElementById(option.value)
+        display()
+        close()
     })
+})
+
+function display() {
+    displayed.toggleAttribute("active");
+}
+
+function close() {
+    sec.removeAttribute("open");
+}
+
+sec.addEventListener("mouseout", close)
+sec.addEventListener("mouseover", e => {
+    sec.setAttribute("open", true)
 })
