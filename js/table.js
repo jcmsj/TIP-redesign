5bd486afe31baea54833472a18b9a5533f264107
Jean Carlo M. San Juan <qjcmsanjuan@tip.edu.ph>
2021-10-04 10:40:35 +0800

    fixed missing logo about page

---
+++ js/table.js
@@ -0,0 +1,19 @@
+
+
+function spawn_table() {
+    
+    return `
+        <table>
+            <tr>
+                <th>School Year</th>
+                <th>Term</th>
+                <th>Enrollment Data</th>
+            </tr>
+            <tr>
+                <td>Summer</td>
+                <td>2nd Semester</td>
+                <td>1st Semester</td>
+            </tr>
+        </table>
+    `
+}
