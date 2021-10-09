f86044284daf0829902eacbe13a7ec90453d6eb7
sjpgarcia <91515796+sjpgarcia@users.noreply.github.com>
2021-10-09 23:00:05 +0800

    Add more content and format code

--- js/carousel.js
+++ js/carousel.js
@@ -1,17 +1,82 @@
 // Our "entry point" element to write content to.
 const carousel = document.getElementById("carousel-parent");
 
-// Static content in the form of a Description" → Image Source mapping.
+// Static content in the form of a Description → [Href, Src] mapping.
 const contents = {
-    "T.I.P. now accepting applicants for second online TCP": "https://tip.edu.ph/index_files/TCP_2_627x377.png",
-    "T.I.P. named 5th top school for engineering research in the country - IRIS": "https://tip.edu.ph/index_files/IRIS_Ranking_628x738.jpg",
-    "T.I.P. continues runner-up streak in Impact 5G Hackathon with carbon-reducing e-commerce app": "https://tip.edu.ph/index_files/Impact_5G_NEW_627x377.png",
-}
+  "T.I.P. now accepting applicants for second online TCP": [
+    "https://tip.edu.ph/article/TIP-now-accepting-applicants-second-online-TCP.html",
+    "https://tip.edu.ph/index_files/TCP_2_627x377.png",
+  ],
+  "T.I.P., SUBICWATER collab for wastewater treatment project in Zambales": [
+    "https://tip.edu.ph/article/TIP-SUBICWATER-collab-wastewater-treatment-project-Zambales.html",
+    "https://tip.edu.ph/index_files/T.I.P._x_SubicWater_628x378.jpg",
+  ],
+  "T.I.P. named 5th top school for engineering research in the country - IRIS":
+    [
+      "https://tip.edu.ph/article/TIP-named-5th-top-school-engineering-research-country-IRIS.html",
+      "https://tip.edu.ph/index_files/IRIS_Ranking_628x738.jpg",
+    ],
+  "T.I.P. continues runner-up streak in Impact 5G Hackathon with carbon-reducing e-commerce app":
+    [
+      "https://tip.edu.ph/article/TIP-continues-runner-up-streak-Impact-5G-Hackathon-carbon-reducing-e-commerce-app.html",
+      "https://tip.edu.ph/index_files/Impact_5G_NEW_627x377.png",
+    ],
+  "T.I.P. ComSci senior clinches 1st runner-up in Impact 5G Hackathon with essay grading app":
+    [
+      "https://tip.edu.ph/article/TIP-ComSci-senior-clinches-1st-runner-up-Impact-5G-Hackathon-essay-grading-app.html",
+      "https://tip.edu.ph/index_files/Eassay_628x377.jpg",
+    ],
+  "T.I.P.ians bag 1st place in industrial engineering conference with undergraduate research paper":
+    [
+      "https://tip.edu.ph/article/TIPians-bag-1st-place-industrial-engineering-conference-undergraduate-research-paper.html",
+      "https://tip.edu.ph/index_files/IEOM2021_628x377.jpg",
+    ],
+  "T.I.P.'s Cleenvent solar-powered ventilation system wins 2nd place in Youthpreneur competition":
+    [
+      "https://tip.edu.ph/article/TIP-Cleenvent-solar-powered-ventilation-system-wins-2nd-place-Youthpreneur-competition.html",
+      "https://tip.edu.ph/index_files/Cleenvent_628x377.jpg",
+    ],
+  "Top caliber professionals grace T.I.P.'s online teaching certificate program":
+    [
+      "https://tip.edu.ph/article/Top-caliber-professionals-grace-TIP-online-teaching-certificate-program.html",
+      "https://tip.edu.ph/index_files/TCP_Lecturers_628x377.png",
+    ],
+  "T.I.P. merges science with business, to offer PSM degree in Engineering Management":
+    [
+      "https://tip.edu.ph/article/TIP-merges-science-business-offer-PSM-degree-Engineering-Management.html",
+      "https://tip.edu.ph/index_files/PSM_EM_628x377.png",
+    ],
+  "T.I.P. student innovations win golds, silvers in first-ever MIPIBIEx": [
+    "https://tip.edu.ph/article/TIP-student-innovations-win-golds-silvers-first-ever-MIPIBIEx.html",
+    "https://tip.edu.ph/index_files/MIPIBIEx_628x377.png",
+  ],
+  "No scraps left behind: T.I.P. engineering senior builds 4-in-1 rechargeable emergency kit from recycled materials":
+    [
+      "https://tip.edu.ph/article/No-scraps-left-behind-TIP-engineering-senior-builds-4-in-1-rechargeable-emergency-kit-recycled-materials.html",
+      "https://tip.edu.ph/index_files/4-in-1_Rechargeable_Emergency_Kit_627x377.png",
+    ],
+  "T.I.P.'s non-invasive bacteria detector on wounds scores podium finish in 2021 James Dyson Awards, advances to internationals":
+    [
+      "https://tip.edu.ph/article/TIP-non-invasive-bacteria-detector-wounds-scores-podium-finish-2021-James-Dyson-Awards-advances-internationals.html",
+      "https://tip.edu.ph/index_files/JDA_627x377.png",
+    ],
+  "Building Careers, Transforming Lives: T.I.P. Career Center celebrates 7 years":
+    [
+      "https://tip.edu.ph/article/Building-Careers-Transforming-Lives-TIP-Career-Center-celebrates-7-years.html",
+      "https://tip.edu.ph/index_files/T.I.P._Career_Center_7th_Anniversary_628x378.jpg",
+    ],
+  "T.I.P. recognizes top performing instructors, best OBTL portfolio in mid-year assembly":
+    [
+      "https://tip.edu.ph/article/TIP-recognizes-top-performing-instructors-best-OBTL-portfolio-mid-year-assembly.html",
+      "https://tip.edu.ph/index_files/Best_Faculty_OBTL_Portfolio_627x377.png",
+    ],
+};
 
 // Create the initial carousel entries.
-Object.entries(contents).forEach(([text, src], index) => {
-    let zIndex = (Object.keys(contents).length - index) * 10
-    carousel.innerHTML += `
+Object.entries(contents).forEach(([text, [href, src]], index) => {
+  let zIndex = (Object.keys(contents).length - index) * 10;
+  carousel.innerHTML += `
+      <a class="carousel-link" href="${href}">
         <div class="carousel-wrapper" style="z-index: ${zIndex}">
           <div class="carousel-content">
             <p>
@@ -20,6 +85,7 @@ Object.entries(contents).forEach(([text, src], index) => {
           </div>
           <img class="carousel-image" alt="" src="${src}"/>
         </div>
+      </a>
     `;
 });
 
@@ -32,39 +98,55 @@ let cursor = 0;
 // Procedure for cycling through carousel entries. See further below for
 // a visual demonstration.
 function cycle() {
-    // If we're not at the last entry.
-    if (cursor + 1 < items.length) {
-        // Fade out the current entry.
-        items[cursor].classList.toggle("fade-out");
-        // and move on to the next one.
-        cursor += 1;
-    } else { // Otherwise
-        // Fade out the current entry.
-        items[cursor].classList.toggle("fade-out");
+  // If we're not at the last entry.
+  if (cursor + 1 < items.length) {
+    // Fade out the current entry.
+    items[cursor].classList.toggle("fade-out");
 
-        // Go to the first entry.
-        cursor = 0 ;
+    // Wait 1.525 seconds to hide the entry.
+    let cursor_ = cursor; // Save the value since we're calling later
+    setTimeout(() => {
+      items[cursor_].classList.toggle("hidden");
+    }, 1525);
 
-        // Fade-in the first entry.
-        items[cursor].classList.toggle("fade-in");
+    // and move on to the next one.
+    cursor += 1;
+  } else {
+    // Otherwise
+    // Fade out the current entry.
+    items[cursor].classList.toggle("fade-out");
 
-        // Wait for 1 second...
-        setTimeout(() => {
-            // Remove existing `fade-out`s from the 2nd entry onwards to
-            // prepare for another cycle.
-            for (item of items.slice(1)) {
-                item.classList.toggle("fade-out");
-            }
+    // Wait 1.525 seconds to hide the entry.
+    let cursor_ = cursor;
+    setTimeout(() => {
+      items[cursor_].classList.toggle("hidden");
+    }, 1525);
 
-            // Wait for 1 second...
-            setTimeout(() => {
-                // Remove `fade-out` and `fade-in` from the 1st entry to
-                // prepare for another cycle.
-                items[0].classList.toggle("fade-out");
-                items[0].classList.toggle("fade-in");
-            }, 1000);
-        }, 1000);
-    }
+    // Fade-in the first entry.
+    items[0].classList.remove("hidden");
+    items[0].classList.toggle("fade-in");
+
+    // Wait for 1 second...
+    setTimeout(() => {
+      // Remove existing `fade-out`s from the 2nd entry onwards to
+      // prepare for another cycle.
+      for (item of items.slice(1)) {
+        item.classList.toggle("fade-out");
+        item.classList.toggle("hidden");
+      }
+
+      // Wait for 1 second...
+      setTimeout(() => {
+        // Remove `fade-out` and `fade-in` from the 1st entry to
+        // prepare for another cycle.
+        items[0].classList.toggle("fade-out");
+        items[0].classList.toggle("fade-in");
+      }, 1000);
+    }, 1000);
+
+    // Go back to the first entry;
+    cursor = 0;
+  }
 }
 
 // To demonstrate, consider:
@@ -109,5 +191,5 @@ function cycle() {
 
 // Periodically cycle the banner every 5 seconds.
 setInterval(() => {
-    cycle()
+  cycle();
 }, 5000);
